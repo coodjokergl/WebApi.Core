bc68aa0c6a94e02a5ee6da1d4eb25c5f79fb0944
龚磊 <Gongl01@mingyuanyun.com>
2019-04-09 10:40:03 +0800

    添加项目文件。

---
+++ MySoftLog.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySoftLog.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
